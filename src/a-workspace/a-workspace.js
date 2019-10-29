330c3cb02e5ca73196b4253497631db07e14417f
j.montoya <j.montoya@bbva.com>
2019-10-29 12:37:31 -0600

    uso de propiedades, condicionales e iteradores

--- src/a-workspace/a-workspace.js
+++ src/a-workspace/a-workspace.js
@@ -4,20 +4,45 @@ class Workspace extends LitElement {
     
     static get properties() {
         return {
-            nameWorkspace: String
+            nameWorkspace: String,
+            names: Array,
+            isSummerDay: Boolean
         }
     }
 
     constructor() {
         super()
         this.nameWorkspace = 'workspace works!'
+        this.names = [
+            'Jose',
+            'Jorge',
+            'Daniel',
+            'David'
+        ]
+        this.isSummerDay = true
     }
 
     render() {
         return html`
+            <h1>${this.nameWorkspace}</h1>
             <div>
-                ${this.nameWorkspace}
+                <h3>Propiedad simple</h3>
+                <span>${this.nameWorkspace}</span>
             </div>
+            <div>
+                <h3>Iteración</h3>
+                ${this.names.map(develop =>html`<li>${develop}</li>`)}
+            </div>
+            <div>
+                <h3>Condicional</h3>
+                ${
+                    this.isSummerDay ? 
+                    html`<span>Deberias dar un paseo</span>` 
+                    : 
+                    html`<span>Podrías descansar en casa</span>`
+                }
+            </div>
+
         `
     }
 
